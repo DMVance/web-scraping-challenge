afa78b92672eed8f0e10ce8854457a3aec3f33c5
DMVance <csljrrt@gmail.com>
2020-11-25 20:02:29 -0600

    final app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,12 +1,19 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from scrape_mars import scrape, get_mongo_dict
 
 app = Flask(__name__)
 
+@app.errorhandler(500)
+def page_not_found(e):
+    # note that we set the 500 status explicitly
+    return render_template('500.html', e=e), 500
+
 @app.route("/")
 def index():
     mongo_dict = get_mongo_dict()
+    if not mongo_dict:
+        abort(500, description="Scrape found no results. Please try again.")
     return render_template("index.html", mongo_dict=mongo_dict)
 
 @app.route("/scraper")
